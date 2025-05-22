9ea23944fe674b614ff8d7a4f737da6fbefa158a
Troy Tristan Jacob <qyorie.0707@gmail.com>
2025-05-22 12:28:32 +0800

    added sortFile function

--- FileSorting/file-sort.cpp
+++ FileSorting/file-sort.cpp
@@ -5,11 +5,26 @@
 using namespace std;
 using namespace std::filesystem;
 
+void sortFile(const path &p, const string ext);
+void createDirectory(const path& p);
 int main(){
 
    path p = current_path();
-   p = p /"jpeg";
    
-   cout << p;
+   sortFile(p, ".txt");
+   
    return 0;
+}
+
+void sortFile(const path& p, const string ext){
+   for (auto const& dir_entry : directory_iterator{p}){
+      if (dir_entry.path().extension() == ext){
+         createDirectory(p);
+         copy_file(dir_entry.path(), p/"text/filetext.txt");
+      }
+   }
+}
+
+void createDirectory(const path& p){
+
 }
