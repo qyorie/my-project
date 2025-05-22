e64b40a3ece68937ff443cf2a7ab5b3df8d2b608
Troy Tristan Jacob <qyorie.0707@gmail.com>
2025-05-22 14:32:42 +0800

    added transferFile function

--- FileSorting/file-sort.cpp
+++ FileSorting/file-sort.cpp
@@ -7,12 +7,15 @@ using namespace std::filesystem;
 
 void sortFile(const path& p, const string& ext, const string& dirName);
 void createDirectory(const path& p, const string dirName);
+void transferFile(const path& src, const path& target);
 int main(){
 
+
    path p = current_path();
-   
+
    sortFile(p, ".txt", "text");
    
+   system("pause");
    return 0;
 }
 
@@ -21,27 +24,34 @@ void sortFile(const path& p, const string& ext, const string& dirName){
    //Iterate through the directory
    for (auto const& dir_entry : directory_iterator{p}){
 
+      //cout << dir_entry.path();
       //checks for file extension name
       if (dir_entry.path().extension() == ext){
          createDirectory(p, dirName);
-         try{
-            copy_file(dir_entry.path(), p/dirName/dir_entry.path().filename());
-         } catch (filesystem_error const& ec){
-            cout << ec.what() << endl;
-         }
+         transferFile(dir_entry.path(), p/dirName/dir_entry.path().filename());
       }
    }
 }
 
 void createDirectory(const path& p, const string dirName){
    
+   //creates directory
    try{
       create_directory(p/dirName);
    }catch (filesystem_error const& ec){
-      ec.what();
+      cout << ec.what() << endl;
    }
 }
 
-void copyFile(const path& p, const string fileName){
-   cout <<"copyfile";
+void transferFile(const path& src, const path& target){
+
+   //transfers file to "target" directory
+   try{
+      copy_file(src, target);
+      remove(src);
+
+      cout << "File/s transferred to " << target.parent_path() << endl;
+   } catch (filesystem_error const& ec){
+      cout << ec.what() << endl;
+   }
 }
