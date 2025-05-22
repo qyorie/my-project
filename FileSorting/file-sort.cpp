2573ec205af9ee79b28c1dedda31f6747b339c91
Troy Tristan Jacob <qyorie.0707@gmail.com>
2025-05-22 13:19:13 +0800

    added createDirectory function

--- FileSorting/file-sort.cpp
+++ FileSorting/file-sort.cpp
@@ -5,26 +5,43 @@
 using namespace std;
 using namespace std::filesystem;
 
-void sortFile(const path &p, const string ext);
-void createDirectory(const path& p);
+void sortFile(const path& p, const string& ext, const string& dirName);
+void createDirectory(const path& p, const string dirName);
 int main(){
 
    path p = current_path();
    
-   sortFile(p, ".txt");
+   sortFile(p, ".txt", "text");
    
    return 0;
 }
 
-void sortFile(const path& p, const string ext){
+void sortFile(const path& p, const string& ext, const string& dirName){
+
+   //Iterate through the directory
    for (auto const& dir_entry : directory_iterator{p}){
       if (dir_entry.path().extension() == ext){
-         createDirectory(p);
-         copy_file(dir_entry.path(), p/"text/filetext.txt");
+         createDirectory(p, "text");
+
+         try{
+            copy_file(dir_entry.path(), p/"text"/dir_entry.path().filename());
+         } catch (filesystem_error const& ec){
+            cout << ec.what() << endl;
+         }
       }
    }
 }
 
-void createDirectory(const path& p){
+void createDirectory(const path& p, const string dirName){
+   
+   
+   try{
+      create_directory(p/dirName);
+   }catch (filesystem_error const& ec){
+      ec.what();
+   }
+}
 
+void copyFile(const path& p, const string fileName){
+   cout <<"copyfile";
 }
